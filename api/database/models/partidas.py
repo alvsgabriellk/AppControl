83af368acee2b14cf8ccb864b42ea7d5b4caad5a
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 10:07:47 -0300

    docs: Corrigindo texto de equipe para equipes na nora da identificação do time

--- api/database/models/partidas.py
+++ api/database/models/partidas.py
@@ -9,7 +9,7 @@ class Partidas(db.Model):
         autoincrement=True, nullable=False
     )
     data = db.Column(db.Date,default=date.today, nullable=False)
-    id_time_casa_fk = db.Column(db.Integer, db.ForeignKey("equipe.id_quipe"))
-    id_time_visitante_fk = db.Column(db.Integer, db.ForeignKey("equipe.id_equipe"))
-    placar_casa = db.Column(db.Integer, nullable=False),
+    id_time_casa_fk = db.Column(db.Integer, db.ForeignKey("equipes.id_equipe"))
+    id_time_visitante_fk = db.Column(db.Integer, db.ForeignKey("equipes.id_equipe"))
+    placar_casa = db.Column(db.Integer, nullable=False)
     placar_visitante = db.Column(db.Integer, nullable=False)
