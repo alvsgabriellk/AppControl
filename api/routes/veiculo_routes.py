6ba0875e8619ea908c3437374b4cd5922d77d25d
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 17:35:59 -0300

    feat: Criado rota pra adicionar novo veiculo

--- api/routes/veiculo_routes.py
+++ api/routes/veiculo_routes.py
@@ -2,3 +2,11 @@ from flask import request, jsonify
 from database import db, Veiculo
 from routes import veic_bp
 from controllers import veiculo_controller 
+
+@veic_bp.route("/novo-veiculo", methods=["POST"])
+def novo_veiculo():
+    dados = request.get_json()
+
+    resposta, status = veiculo_controller(dados)
+
+    return jsonify(resposta), status
