9dbc8264d7b8ca90e72f9495e28b5d25839dc859
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 17:37:52 -0300

    build: Importando arquivo de rota no init de routes

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -4,3 +4,5 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 veic_bp = Blueprint("veic", __name__, url_prefix="/veic")
 
 from .auth import register_routes
+
+from routes import veiculo_routes
