2f2388ec8bcdc28992b174065717baa602487532
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 13:57:32 -0300

    feat: Criado variaveis pra pegar e enviar os dados vindo de routes

--- api/controllers/auth/register_controller.py
+++ api/controllers/auth/register_controller.py
@@ -1,1 +1,8 @@
 from services import register_ok
+
+def register_dados(dados):
+    nome = dados["nome"]
+    email = dados["email"]
+    senha = dados["senha"]
+    
+    return register_ok(nome, email, senha)
