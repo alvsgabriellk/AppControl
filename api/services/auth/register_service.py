0bd0fe688ad37b24619d02838e2cc2f91910a6e6
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 13:54:15 -0300

    feat: Service do registro do usuário criado, com retorno de erro ou resposta ok

--- api/services/auth/register_service.py
+++ api/services/auth/register_service.py
@@ -1,3 +1,22 @@
 from database import db, Usuario
 from utils import gerar_senha_hash
 from sqlalchemy.exc import IntegrityError
+
+def register_ok(nome, email, senha):
+    senha_hash = gerar_senha_hash(senha)
+    
+    usuario = Usuario(
+        nome=nome,
+        email=email,
+        senha=senha_hash
+    )
+
+    try:
+        db.session.add(usuario)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+
+        return {"error": "Email já está sendo usado!"}, 409
+    
+    return {"msg": "Usuário cadastrado!"}, 201
