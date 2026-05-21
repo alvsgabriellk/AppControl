b21f6c4c61a50470969144ad31d69954f61ffb02
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 14:01:01 -0300

    feat: Rota de cadastro do usuário criada

--- api/routes/auth/register_routes.py
+++ api/routes/auth/register_routes.py
@@ -2,3 +2,11 @@ from flask import request, jsonify
 from database import db, Usuario
 from routes import auth_bp
 from controllers import register_dados
+
+@auth_bp.route("/sign-up", methods=["POST"])
+def cadastro_usuario():
+    dados = request.get_json()
+
+    resposta, status = register_dados(dados)
+
+    return jsonify({"msg": resposta}), status
