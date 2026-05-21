34688312ec72180556e3e5b8a9e720fa16678b9b
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 10:20:22 -0300

    config: Adicionando reconhecimento de desenvolvimento, e aceitando url com cors pra usar a api

--- api/app.py
+++ api/app.py
@@ -6,11 +6,19 @@ from config import DesenvolvimentoConfig, ProducaoConfig
 
 app = Flask(__name__)
 
+ENV = os.getenv("ENV", "local")
+if ENV == "production":
+    app.config.from_object(ProducaoConfig)
+else:
+    app.config.from_object(DesenvolvimentoConfig)
+
+CORS(app, origins=app.config["CORS_ORIGINS"])
+
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config.get("DEBUG"))
 
