32b6041274d7224f40e0bddc8a2c91c8aa87082e
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 17:15:47 -0300

    feat: Criando rota blueprint pra crud de veiculos

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+veic_bp = Blueprint("veic", __name__, url_prefix="/veic")
 
 from .auth import register_routes
