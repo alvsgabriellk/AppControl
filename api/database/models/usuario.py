a2739715d1ee1413b8782f17483bea2ef1d31b02
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 13:46:45 -0300

    feat: Tábela de usuários criada

--- api/database/models/usuario.py
+++ api/database/models/usuario.py
@@ -1,2 +1,13 @@
 from database import db
 from datetime import datetime
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(35), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(256), nullable=False)
+    data_criado = db.Column(db.DateTime, default=datetime.utcnow)
+    data_ultima_atualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    ativo = db.Column(db.Boolean, default=True)
