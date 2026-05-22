e25c9ec7a84c4517985662fdb7536cf74fb2c45f
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 22:44:05 -0300

    perf: Melhorando coluna de nome da tabela de oficina

--- api/database/models/oficina.py
+++ api/database/models/oficina.py
@@ -5,7 +5,7 @@ class Oficina(db.Model):
     __tablename__ = "oficinas"
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100),nullable=False)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
     telefone = db.Column(db.String(20))
     responsavel = db.Column(db.String(100))
     endereco = db.Column(db.String(200))
