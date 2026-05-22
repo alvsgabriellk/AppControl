e43298892a9b50933d07196e0fa34de2bf156edf
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 22:19:44 -0300

    feat Tabela de oficina criado, com relacionamento no futuro com a tabela de manuntenções

--- api/database/models/oficina.py
+++ api/database/models/oficina.py
@@ -1,2 +1,21 @@
 from database import db
 from datetime import datetime
+
+class Oficina(db.Model):
+    __tablename__ = "oficinas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100),nullable=False)
+    telefone = db.Column(db.String(20))
+    responsavel = db.Column(db.String(100))
+    endereco = db.Column(db.String(200))
+    cidade = db.Column(db.String(100))
+    estado = db.Column(db.String(50))
+    data_cadastro = db.Column(db.DateTime,default=datetime.utcnow)
+    
+    # RELACIONAMENTO
+    manutencoes = db.relationship(
+        "Manutencao",
+        backref="oficina",
+        lazy=True
+    )
