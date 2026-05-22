fda987b979a1d9a773ea698db47f20e87ca2716f
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 22:33:52 -0300

    feat: Controller de oficina criado pra receber e entregar dados de oficina

--- api/controllers/oficina_controller.py
+++ api/controllers/oficina_controller.py
@@ -1,1 +1,15 @@
 from services import oficina_ok
+
+def oficina_dados(dados):
+    nome = dados["nome"]
+    telefone = dados=["telefone"]
+    responsavel = dados["responsavel"]
+    endereco = dados["endereco"]
+    cidade = dados["cidade"]
+    estado = dados["estado"]
+
+    return oficina_ok(
+        nome, telefone,
+        responsavel, endereco,
+        cidade, estado
+    )
