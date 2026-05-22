74f7254760a6bd5d4307bc037ae04b7519a3b552
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 23:22:36 -0300

    feat: Criado controller pra receber e enviar dados de manutencao

--- api/controllers/manutencao_controller.py
+++ api/controllers/manutencao_controller.py
@@ -1,1 +1,16 @@
 from services import manutencao_ok
+
+def manutencao_dados(dados):
+
+    veiculo_id = dados["veiculo_id"]
+    oficina_id = dados["oficina_id"]
+    tipo_manutencao = dados["tipo_manutencao"]
+    valor = float(dados["valor"])
+    km_manutencao = float(dados["km_manutencao"])
+    garantia_dias = int(dados["garantia_dias"])
+
+    return manutencao_ok(
+        veiculo_id, oficina_id,
+        tipo_manutencao, valor,
+        km_manutencao, garantia_dias
+    )
