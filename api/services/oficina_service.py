97ee01ed690be5c3a6a2f38b3bd23b17f24e9d63
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 22:41:47 -0300

    feat: Service de oficina criado, com retorno de resposta ok ou de erro

--- api/services/oficina_service.py
+++ api/services/oficina_service.py
@@ -1,3 +1,24 @@
 
 from database import db, Oficina
 from sqlalchemy.exc import IntegrityError
+
+def oficina_ok(nome, telefone, responsavel, endereco, cidade, estado):
+    oficina = Oficina(
+        nome=nome,
+        telefone=telefone,
+        responsavel=responsavel,
+        endereco=endereco,
+        cidade=cidade,
+        estado=estado
+    )
+
+    try:
+        db.session.add(oficina)
+        db.session.commit()
+    
+    except IntegrityError:
+        db.session.rollback()
+
+        return {"error": "Oficina já foi cadastrada"}, 409
+    
+    return {"msg": "Oficina cadastrada!"}, 201
