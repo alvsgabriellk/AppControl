51be4b36c3c0090ce0a01408cb32c9598014ec83
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 23:15:26 -0300

    feat: Rota de manutencao criada

--- api/routes/manutencao_routes.py
+++ api/routes/manutencao_routes.py
@@ -1,3 +1,12 @@
 from flask import request, jsonify
 from controllers import manutencao_dados
 from routes import mant_bp
+
+@mant_bp.route("/nova-manutencao", methods=["POST"])
+def nova_manutencao():
+    dados = request.get_json()
+
+    resposta, status = manutencao_dados(dados)
+
+    return jsonify(resposta), status
+
