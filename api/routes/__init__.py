87825bba535e32cd0891a22fc89892faedf06843
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 23:11:55 -0300

    feat: Criado conexão com blueprint pra rota de manutencoes

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 veic_bp = Blueprint("veic", __name__, url_prefix="/veic")
 ofic_bp = Blueprint("ofic", __name__, url_prefix="/ofic")
+mant_bp = Blueprint("mant", __name__, url_prefix="/mant")
 
 from .auth import register_routes
 from routes import veiculo_routes
