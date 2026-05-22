e042a663731ae0324154a9e3fe8019c7402a13d5
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 21:55:25 -0300

    perf: Alterando nome de arquivo para importação adequada

--- api/routes/veiculo_routes.py
+++ api/routes/veiculo_routes.py
@@ -1,11 +1,11 @@
 from flask import request, jsonify
 from routes import veic_bp
-from controllers import veiculo_controller 
+from controllers import veiculos_dados 
 
 @veic_bp.route("/novo-veiculo", methods=["POST"])
 def novo_veiculo():
     dados = request.get_json()
 
-    resposta, status = veiculo_controller(dados)
+    resposta, status = veiculos_dados(dados)
 
     return jsonify(resposta), status
