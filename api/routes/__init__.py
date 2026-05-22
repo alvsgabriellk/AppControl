eca4acd1cb7a2cf42f7345a7898679cbe7776e56
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 22:23:13 -0300

    feat: Criando rota de conexão para crud de oficina

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 veic_bp = Blueprint("veic", __name__, url_prefix="/veic")
+ofic_bp = Blueprint("ofic", __name__, url_prefix="/ofic")
 
 from .auth import register_routes
-
 from routes import veiculo_routes
