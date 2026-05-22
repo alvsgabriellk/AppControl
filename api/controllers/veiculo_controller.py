41431148740d824a5fb2f48b688cc697749b1fb6
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-21 21:57:14 -0300

    fix: Corrigindo erro de recebimento de dados errado, certo para dict -> []

--- api/controllers/veiculo_controller.py
+++ api/controllers/veiculo_controller.py
@@ -2,13 +2,13 @@ from services import veiculo_ok
 
 def veiculos_dados(dados):
 
-    usuario_id = dados("usuario_id")
-    placa = dados("placa")
-    renavan = dados("renavan")
-    marca = dados("marca")
-    modelo = dados("modelo")
-    km_compra = float(dados("km_compra"))
-    km_atual = float(dados("km_atual"))
+    usuario_id = dados["usuario_id"]
+    placa = dados["placa"]
+    renavan = dados["renavan"]
+    marca = dados["marca"]
+    modelo = dados["modelo"]
+    km_compra = float(dados["km_compra"])
+    km_atual = float(dados["km_atual"])
 
     return veiculo_ok(
         usuario_id, placa, 
