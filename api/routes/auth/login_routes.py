f8e13dc8c93f237431188b6861bab84342096fcc
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-24 13:33:22 -0300

    feat: Criado rota de login

--- api/routes/auth/login_routes.py
+++ api/routes/auth/login_routes.py
@@ -1,3 +1,11 @@
 from flask import request, jsonify
 from routes import auth_bp
 from controllers import login_dados
+
+@auth_bp.route("/sign-in", methods=["POST"])
+def login():
+dados = request.get_json()
+
+resposta, status = login_dados(dados)
+
+return jsonify(resposta), status
