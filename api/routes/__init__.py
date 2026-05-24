e500d7ae737cc5f61cccd7998db7e4e30d0fab86
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-24 13:35:31 -0300

    Importando rota de login no init de controllers

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -4,6 +4,6 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 veic_bp = Blueprint("veic", __name__, url_prefix="/veic")
 ofic_bp = Blueprint("ofic", __name__, url_prefix="/ofic")
 
-from .auth import register_routes
+from .auth import register_routes, login_routes
 from routes import veiculo_routes
 from routes import oficina_routes
