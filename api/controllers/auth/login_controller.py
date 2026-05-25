193b5cd10dd8b3de3be45a547d0ae0831d36b19e
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 13:50:41 -0300

    perf: Removendo parâmetro/dado que não será utilizado nesse tipo de rota

--- api/controllers/auth/login_controller.py
+++ api/controllers/auth/login_controller.py
@@ -1,8 +1,7 @@
 from services import login_ok
 
 def login_dados(dados):
-    nome = dados["nome"]
     email = dados["email"]
     senha = dados["senha"]
 
-    return login_ok(nome, email, senha)
+    return login_ok(email, senha)
