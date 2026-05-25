5d34d4acff820283b582e28498fb2c9a4ebbbde6
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 13:38:55 -0300

    build: Importando o arquivo do controller de login no init de controllers

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,3 +1,4 @@
 from .auth.register_controller import register_dados
+from .auth.login_controller import login_dados
 from .veiculo_controller import veiculos_dados
 from .oficina_controller import oficina_dados
