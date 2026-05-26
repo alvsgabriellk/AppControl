36473e9fd5f78d8a8eafca1142098e2b72ad86de
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 21:57:03 -0300

    build: Importando função necessário para a rota de atualizar veiculo funcionar

--- api/routes/veiculo_routes.py
+++ api/routes/veiculo_routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from routes import veic_bp
-from controllers import veiculos_dados, veiculos_lista, veiculos_remover
+from controllers import veiculos_dados, veiculos_lista, veiculos_remover, veiculos_atualizar
 
 @veic_bp.route("/novo-veiculo", methods=["POST"])
 def novo_veiculo():
