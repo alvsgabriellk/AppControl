117ca4785b87065be35dc6541f1452de867d81eb
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 18:05:16 -0300

    feat: Criado controller de lista, remover, atualizar e buscar para manutenção

--- api/controllers/manutencao_controller.py
+++ api/controllers/manutencao_controller.py
@@ -20,3 +20,22 @@ def manutencao_dados(dados):
         tipo_manutencao, valor,
         km_manutencao, garantia_dias
     )
+
+def manutencoes_lista():
+    return manutencoes_lista_ok()
+
+
+def manutencoes_remover(dados):
+    id = dados["id"]
+
+    return manutencoes_remover_ok(id)
+
+
+def manutencoes_atualizar(dados):
+    return manutencoes_atualizar_ok(dados)
+
+
+def manutencoes_buscar(dados):
+    id = dados["id"]
+
+    return manutencoes_buscar_ok(id)
