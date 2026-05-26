c12847781f0ffdb21ff9ba3cce0d4b06554caf3b
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 21:43:45 -0300

    perf: Corrigindo importação errada

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,5 +1,5 @@
 from .auth.register_controller import register_dados
-from .veiculo_controller import veiculos_dados, veiculos_lista, veiculos_remover_ok
+from .veiculo_controller import veiculos_dados, veiculos_lista, veiculos_remover
 from .auth.login_controller import login_dados
 from .veiculo_controller import veiculos_dados
 from .oficina_controller import oficina_dados
