ed8b91fe40b5836a3fed496fe275a6e40bf84242
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 19:08:35 -0300

    build: Importando funções de oficina para o init de controllers

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -8,7 +8,13 @@ from .veiculo_controller import (
 )
 from .auth.login_controller import login_dados
 from .veiculo_controller import veiculos_dados
-from .oficina_controller import oficina_dados
+from .oficina_controller import (
+    oficina_dados,
+    oficinas_lista,
+    oficinas_buscar,
+    oficinas_remover,
+    oficinas_atualizar
+)
 from .manutencao_controller import (
     manutencao_dados,
     manutencoes_lista,
