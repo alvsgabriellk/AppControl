e4591e4e4fcd27b7b3b712d2bed1f331c9f06bce
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 18:46:59 -0300

    feat: Criado rotas pra oficina pra buscar, listar, remover e atualizar

--- api/routes/oficina_routes.py
+++ api/routes/oficina_routes.py
@@ -15,3 +15,37 @@ def nova_oficina():
     resposta, status = oficina_dados(dados)
 
     return jsonify(resposta), status
+
+@ofic_bp.route("/oficinas", methods=["GET"])
+def listar_oficinas():
+
+    resposta, status = oficinas_lista()
+
+    return jsonify(resposta), status
+
+
+@ofic_bp.route("/remover", methods=["DELETE"])
+def remover_oficina():
+    dados = request.get_json()
+
+    resposta, status = oficinas_remover(dados)
+
+    return jsonify(resposta), status
+
+
+@ofic_bp.route("/atualizar", methods=["PUT"])
+def atualizar_oficina():
+    dados = request.get_json()
+
+    resposta, status = oficinas_atualizar(dados)
+
+    return jsonify(resposta), status
+
+
+@ofic_bp.route("/buscar", methods=["GET"])
+def buscar_oficina():
+    dados = request.get_json()
+
+    resposta, status = oficinas_buscar(dados)
+
+    return jsonify(resposta), status
