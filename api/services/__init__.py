a8adcf1656d3cd90be2cdabb2ad9d45b5926e049
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 21:11:53 -0300

    build: Importando lista de veiculos pro init de services

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -1,4 +1,4 @@
 from .auth.register_service import register_ok
-from .veiculo_service import veiculo_ok
+from .veiculo_service import veiculo_ok, veiculos_lista_ok
 from .oficina_service import oficina_ok
 from .manutencao_service import manutencao_ok
