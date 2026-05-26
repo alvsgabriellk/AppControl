a980728ab2520c94b8c7c645f9c9d5d9c7362fa8
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 09:27:10 -0300

    build: Importando função necessária parao controller de buscar veiculos

--- api/controllers/veiculo_controller.py
+++ api/controllers/veiculo_controller.py
@@ -1,4 +1,4 @@
-from services import veiculo_ok, veiculos_lista_ok, veiculos_remover_ok, veiculos_atualizar_ok
+from services import veiculo_ok, veiculos_lista_ok, veiculos_remover_ok, veiculos_atualizar_ok, veiculos_buscar_ok
 
 def veiculos_dados(dados):
 
