b79f9ce54cdf15e709454607c7efc2a179488a79
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-25 22:42:53 -0300

    build: Importando função de services no controller

--- api/controllers/veiculo_controller.py
+++ api/controllers/veiculo_controller.py
@@ -1,4 +1,4 @@
-from services import veiculo_ok, veiculos_lista_ok, veiculos_remover_ok
+from services import veiculo_ok, veiculos_lista_ok, veiculos_remover_ok, veiculos_atualizar_ok
 
 def veiculos_dados(dados):
 
