5495359285035138239369fafccadc025da999e0
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 09:24:55 -0300

    build: Importando função necessária para a rota de buscar veículo

--- api/routes/veiculo_routes.py
+++ api/routes/veiculo_routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from routes import veic_bp
-from controllers import veiculos_dados, veiculos_lista, veiculos_remover, veiculos_atualizar
+from controllers import veiculos_dados, veiculos_lista, veiculos_remover, veiculos_atualizar, veiculos_buscar
 
 @veic_bp.route("/novo-veiculo", methods=["POST"])
 def novo_veiculo():
