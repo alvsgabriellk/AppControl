bfc25876c35e0ed97387ed3960bee9b7d1656a74
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-27 19:24:59 -0300

    build: Importando bibs e arquivos necessários para as rotas de esqueci senha e resetar senha

--- api/routes/auth/email_routes.py
+++ api/routes/auth/email_routes.py
@@ -1,7 +1,9 @@
 from routes import auth_bp
-from utils import validar_token
+from services import enviar_recuperacao
+from utils import validar_token, gerar_senha_hash
 from database import Usuario, db
 from sqlalchemy import select
+from flask import request
 
 @auth_bp.route("/confirmar-email/<token>")
 def confirmar_email(token):
