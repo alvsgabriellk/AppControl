e598611ae540e5bd8c69ce795321b586a3685c51
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-27 19:46:03 -0300

    feat: Criado service para enviar email de recuperação de senha

--- api/services/auth/email_service.py
+++ api/services/auth/email_service.py
@@ -22,3 +22,25 @@ def enviar_confirmacao(usuario):
     """
 
     mail.send(msg)
+
+
+def enviar_recuperacao(usuario):
+
+    token = gerar_token(usuario.email)
+
+    link = f"http://localhost:5000/auth/resetar-senha/{token}"
+
+    msg = Message(
+        subject="Recuperar senha",
+        sender=current_app.config["MAIL_USERNAME"],
+        recipients=[usuario.email]
+    )
+
+    msg.body = f"""
+    Olá, {usuario.nome}!
+    Clique no link abaixo para resetar sua senha
+    {link}
+    Caso você não tenha solicitado o envio de recuperação de senha, então desconsidere este email.
+    """
+
+    mail.send(msg)
