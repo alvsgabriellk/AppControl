8dc0534835192b89efc1ea278a802656a4b00be5
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-26 22:41:04 -0300

    config: Criado conexão com dados de email para a configuração

--- api/config.py
+++ api/config.py
@@ -6,6 +6,15 @@ load_dotenv()
 class Config:
     KEY_API = os.getenv("KEY_API")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
 
 
 class DesenvolvimentoConfig(Config):
